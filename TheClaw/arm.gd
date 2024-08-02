99b0781455cffe02f141a0af19b1f77158c42d1a
Weston <westonkelliher@gmail.com>
2024-08-02 03:55:27 -0700

    multi-tiered arm

---
+++ TheClaw/arm.gd
@@ -0,0 +1,51 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var L_l := Input.get_action_raw_strength("LS_left")
+	var L_r := Input.get_action_raw_strength("LS_right")
+	var L_u := Input.get_action_raw_strength("LS_up")
+	var L_d := Input.get_action_raw_strength("LS_down")
+	var L_t0 := Input.get_action_raw_strength("LT0")
+	var L_t1 := Input.get_action_raw_strength("LT1")
+	#
+	var R_l := Input.get_action_raw_strength("RS_left")
+	var R_r := Input.get_action_raw_strength("RS_right")
+	var R_u := Input.get_action_raw_strength("RS_up")
+	var R_d := Input.get_action_raw_strength("RS_down")
+	var R_t0 := Input.get_action_raw_strength("RT0")
+	var R_t1 := Input.get_action_raw_strength("RT1")
+	#
+	var L_stick := correct_and_convert_stick(L_l, L_r, L_u, L_d)
+	var L_trig := correct_and_convert_trigger(L_t0, L_t1)
+	var R_stick := correct_and_convert_stick(R_l, R_r, R_u, R_d)
+	var R_trig := correct_and_convert_trigger(R_t0, R_t1)
+	#
+	$BaseArm.handle_input(L_stick, L_trig, delta)
+	$BaseArm/ForeArm.handle_input(R_stick, R_trig, delta)
+	#
+	#
+	#$ForeArm.basis = $BaseArm/Arm.basis
+
+
+
+func correct_and_convert_stick(stick_l: float, stick_r: float, stick_u: float, stick_d: float) -> Vector2:
+	var stick_totals := pow((0.01 + stick_l + stick_r + stick_u + stick_d), .9)
+	var raw_x := stick_r - stick_l
+	var raw_y := stick_u - stick_d
+	var raw_diff: float = abs(abs(raw_x) - abs(raw_y))
+	var final_x := raw_x / (1 + pow(1 - raw_diff, 1.5)*0.3)
+	var final_y := raw_y / (1 + pow(1 - raw_diff, 1.5)*0.3)
+	return Vector2(final_x, final_y)
+
+func correct_and_convert_trigger(lt0: float, lt1: float) -> float:
+	var trig := (1.0 - lt0 + lt1)/2.0
+	if lt0 == 0 && lt1 == 0:
+		trig = 0
+	return trig
--- TheClaw/arm_segment.gd
+++ TheClaw/arm_segment.gd
@@ -1,11 +1,28 @@
 extends Node3D
 
 
-const MAX_ANGLE_FROM_STICK := 30.0
-const MAX_ANGLE_FROM_TRIG := 40.0
-const MAX_ANGULAR_SPEED := 8.0
+@export var MAX_ANGLE_FROM_STICK := 30.0
+@export var MAX_ANGLE_FROM_TRIG := 40.0
+@export var LENGTH := 1.8 :
+	set(value):
+		LENGTH = value
+		$Arm/Shape.shape = $Arm/Shape.shape.duplicate()
+		$Arm/Shape.shape.height = LENGTH+0.2
+		$Arm/Shape.position.y = LENGTH/2.0
+		$Arm/Mesh.mesh = $Arm/Mesh.mesh.duplicate()
+		$Arm/Mesh.mesh.height = LENGTH+0.2
+		$Arm/Mesh.position.y = LENGTH/2.0
+		$Arm/End.position.y = LENGTH
+	get:
+		return LENGTH
+
+
+const MAX_ANGULAR_SPEED := 6.0
+const MIN_ANGULAR_SPEED := 1.0
 
 const REMEMBER_THRESHOLD := 0.1
+
+
 
 var remembered_stick_vec := Vector2.RIGHT
 var arm_vector := Vector3.UP
@@ -26,8 +43,6 @@ func handle_input(stick_position: Vector2, trig_level: float, delta: float) -> v
 		var lerp_amnt := 1 - pow(0.000001, delta)
 		remembered_stick_vec = remembered_stick_vec.lerp(stick_vec.normalized(), lerp_amnt)
 	var stick_angle := stick_vec.angle()
-	print(stick_vec)
-	print(stick_angle)
 	var stick_intensity := stick_vec.length()
 	var stick_axis := Vector3.RIGHT.rotated(Vector3.UP, stick_angle - PI/2)
 	var proj_2d := Vector2.RIGHT.rotated(stick_angle)
@@ -48,13 +63,15 @@ func handle_input(stick_position: Vector2, trig_level: float, delta: float) -> v
 	var top_basis := Vector3.UP.rotated(full_axis, full_rot)
 	var forward_basis := Vector3.BACK.rotated(full_axis, full_rot)
 	var right_basis := top_basis.cross(forward_basis).normalized()
-	$BaseArm.basis = Basis(right_basis, top_basis, forward_basis).orthonormalized()
+	basis = Basis(right_basis, top_basis, forward_basis).orthonormalized()
 	###
 
 func move_toward_target(target: Vector3, delta: float) -> void:
-	var angle := MAX_ANGULAR_SPEED*delta
-	if arm_vector.angle_to(target) < angle:
+	var angular_speed := MIN_ANGULAR_SPEED + 40.0*pow(arm_vector.angle_to(target), 3)
+	if angular_speed > MAX_ANGULAR_SPEED:
+		angular_speed = MAX_ANGULAR_SPEED
+	if arm_vector.angle_to(target) < angular_speed*delta:
 		arm_vector = target
 		return
 	var axis := arm_vector.cross(target).normalized()
-	arm_vector = arm_vector.rotated(axis, angle)
+	arm_vector = arm_vector.rotated(axis, angular_speed*delta)
