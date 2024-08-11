7d73d640c0c93a54e6c53cd94112a3df3e4828f0
Weston <westonkelliher@gmail.com>
2024-08-11 14:30:50 -0700

    nesting blocks and adding slots works now (hbox only)

--- Slotting/block.gd
+++ Slotting/block.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_appender_activated() -> void:
+	var new_slot = load("res://slot.tscn").instantiate()
+	$HBoxContainer.add_child(new_slot)
+	$HBoxContainer.move_child(new_slot, -2)
